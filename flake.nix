a57a3447ff45b40dcb691ecd4d8e7af384973522
lokaimoma <kelvinclark2k77@gmail.com>
2026-07-18 12:04:28 +0100

    use localized flake dev shell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cmake
+          gcc
+        ];
+
+        shellHook = ''
+          # exec zsh
+        '';
+      };
+    };
+}
